dfa03c7346fc97d12b89fc743a474813d9e2dd10
milkandhoney995 <harada.g17@gmail.com>
2025-06-13 22:57:14 +0900

    チェックボックス

--- app/components/CheckboxController.tsx
+++ app/components/CheckboxController.tsx
@@ -1,18 +1,45 @@
 import React from 'react';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import { DynamicGuestField } from '@/types/DynamicGuestField';
+import { Controller, Control, FieldValues, Path } from 'react-hook-form';
 
-type CheckboxControllerProps = {
+interface CheckboxControllerProps<T extends FieldValues> {
+  name: DynamicGuestField
+  control: Control<T>;
   label: string;
-  checked: boolean;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-const CheckboxController: React.FC<CheckboxControllerProps> = ({ label, checked, onChange }) => {
+const CheckboxController = <T extends FieldValues>({
+  name,
+  control,
+  label,
+  onChange
+}: CheckboxControllerProps<T>) => {
   return (
-    <FormControlLabel
-      control={<Checkbox checked={checked} onChange={onChange} />}
-      label={label}
+    <Controller
+      name={name as Path<T>}
+      control={control}
+      render={({ field }) => (
+        <>
+          <FormControlLabel
+            control={
+              <Checkbox
+                {...field}
+                checked={field.value}
+                onChange={(e) => {
+                  const value = e.target.value === 'true';
+                  field.onChange(value)
+                  if (onChange) onChange(e)
+                }}
+              />
+            }
+            label={label}
+            required
+          />
+        </>
+      )}
     />
   );
 };
